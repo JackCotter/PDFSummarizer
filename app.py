d81f97e0468d187accef912d8c9189370a60ba43
JackCotter <79673714+JackCotter@users.noreply.github.com>
2024-01-15 12:37:40 -0800

    added cli args, checking

--- app.py
+++ app.py
@@ -4,15 +4,31 @@ import numpy as np
 from pdf2image import convert_from_path
 import os
 from dotenv import load_dotenv
+import sys
 
-load_dotenv()
-pytesseract.pytesseract.tesseract_cmd = os.environ.get('TESSERACT_PATH')
+def convert_pdf_to_text(path_to_pdf):
+  load_dotenv()
+  pytesseract.pytesseract.tesseract_cmd = os.environ.get('TESSERACT_PATH')
 
-images = convert_from_path('', poppler_path=os.environ['POPPLER_PATH'])
-total_text = ""
-for image in images:
-  text = pytesseract.image_to_string(images[1], lang='eng')
-  total_text += text
-  break
+  images = convert_from_path(path_to_pdf, poppler_path=os.environ['POPPLER_PATH'])
+  total_text = ''
+  for image in images:
+    text = pytesseract.image_to_string(image, lang='eng')
+    total_text += text
 
-print(total_text)
+  print(total_text)
+
+if __name__ == '__main__':
+  if len(sys.argv) < 2:
+    print('Please provide a path to a PDF file')
+    exit(1)
+
+  if not os.path.exists(sys.argv[1]):
+    print('File does not exist')
+    exit(1)
+
+  if not sys.argv[1].endswith('.pdf'):
+    print('File is not a PDF')
+    exit(1)
+
+  convert_pdf_to_text(sys.argv[1])
