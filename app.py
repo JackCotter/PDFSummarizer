1b89a3db70ff0098638f900810281528f3dba3c9
JackCotter <79673714+JackCotter@users.noreply.github.com>
2024-01-15 16:55:37 -0800

    added pdf start and end flags

--- app.py
+++ app.py
@@ -5,17 +5,35 @@ from pdf2image import convert_from_path
 import os
 from dotenv import load_dotenv
 import sys
+from openai import OpenAI
+import argparse
+
+load_dotenv()
+client = OpenAI()
 
 def convert_pdf_to_text(path_to_pdf):
-  load_dotenv()
   pytesseract.pytesseract.tesseract_cmd = os.environ.get('TESSERACT_PATH')
+  args = parser.parse_args()
+
+  pdf_start = args.s if args.s else 0
+  pdf_finish = args.f if args.f else None
 
   images = convert_from_path(path_to_pdf, poppler_path=os.environ['POPPLER_PATH'])
+  images = images[pdf_start:pdf_finish]
+
   total_text = ''
   for image in images:
     text = pytesseract.image_to_string(image, lang='eng')
     total_text += text
 
+  # completion = client.chat.completions.create(
+  #   model="gpt-3.5-turbo",
+  #   messages=[
+  #     {"role": "system", "content": "You are a skilled software engineer"},
+  #     {"role": "user", "content": "please tell me a joke"}
+  #   ]
+  # )
+  # print(completion.choices[0].text)
   print(total_text)
 
 if __name__ == '__main__':
@@ -32,3 +50,8 @@ if __name__ == '__main__':
     exit(1)
 
   convert_pdf_to_text(sys.argv[1])
+  parser = argparse.ArgumentParser(description="")
+
+  parser.add_argument('-s', type=int, help="Specify an integer value after -s")
+  parser.add_argument('-f', type=int, help="Specify an integer value after -s")
+
